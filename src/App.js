9cafb1f44c179b30784bdc505ed81ee17a42545d
Rodrigo M <69582729+rodri48@users.noreply.github.com>
2021-04-16 17:06:30 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navegacion from "./components/Navegacion";
+
+function App() {
+  return (
+    <div className="App">
+      <Navegacion />
+    </div>
+  );
+}
+
+export default App;
